128edf9fb07c87cc9c08b487279bf7934f5bfa14
Shubham <shubhamgupta0720@gmail.com>
2024-04-09 22:37:35 +0530

    Changes

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import moon from "./src/assets/images/icon-moon.svg"
+import moon from "../assets/images/icon-moon.svg"
 
 function ToDo(){
     return (
-        <div className="header-theme">
-            <h1>TODO</h1>
-            <img src={moon} alt="" />
+        <div className="main-panel">
+            <div className="header-theme">
+                <h1>TODO</h1>
+                <img src={moon} alt="" />
+            </div>
         </div>
     )
 }
